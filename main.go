8d425a3bd1d0e8a84f42acbc52bbda484557ec41
SebasCalderon27 <168150875+SebasCalderon27@users.noreply.github.com>
2025-10-24 12:42:46 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+type Producto struct {
+	ID       int
+	Nombre   string
+	Precio   float64
+	Cantidad int
+}
+
+func NuevoProducto(id int, name string, prec float64, cant int) *Producto {
+	return &Producto{
+		ID:       id,
+		Nombre:   name,
+		Precio:   prec,
+		Cantidad: cant,
+	}
+	
+}
+
+func (p *Producto) ActualizarPrecio(NuevoPrecio float64) {
+	p.Precio = NuevoPrecio
+}
+
+func (p *Producto) ActualizarCantidad(NuevaCantidad int) {
+	p.Cantidad = NuevaCantidad
+}
+
+func (p Producto) MostrarInfo() {
+	fmt.Println("El ID de el producto es: ", p.ID)
+
+	fmt.Println("El NOMBRE de el producto es: ", p.Nombre)
+
+	fmt.Println("El PRECIO de el producto es: ", p.Precio)
+
+	fmt.Println("El CANTIDAD de el producto es: ", p.Cantidad)
+}
+
+type Inventario struct{
+	Productos map[int]Producto
+}
+
+func NuevoInventario()Inventario{
+	return Inventario{
+		Productos: map[int]Producto{},
+	}
+}
+
+func (inve*Inventario)AgregarProducto(p Producto){
+	p.ID+=1
+	inve.Productos[p.ID]=p
+}
+
+func (inve*Inventario)EliminarProducto(p Producto){
+	p,existencia := inve.Productos[p.ID]
+	if existencia == true{
+		delete(inve.Productos,p.ID)
+	}
+}
+
